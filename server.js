9f2b59275d9740101eda0f65733e224bcc003464
ayoub <ayoub.belghar@gmail.com>
2018-06-17 18:00:02 +0100

    connect to mongoose

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ var io = require('socket.io')(http);
 var mongoose = require('mongoose');
 const URI = "mongodb://sinchi:3afritto@ds261540.mlab.com:61540/my-chilf-app";
 
-mongoose.connect(URI, _ => {
-  console.log("connection OK");
+const options = {
+  useMongoClient: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
+mongoose.connect(URI, options, _ => {
+  console.log("Conenction Success");
 });
 app.use(express.static(__dirname + '/public/index.html'));
 
