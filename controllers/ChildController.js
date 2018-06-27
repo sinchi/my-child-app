7900dadb99153ac62f282531b822d0d02b9751f3
ayoub <ayoub.belghar@gmail.com>
2018-06-27 22:25:29 +0100

    add to database

--- controllers/ChildController.js
+++ controllers/ChildController.js
@@ -19,6 +19,7 @@ exports.addChild = function(req, res, next){
 
 exports.addLieuVisite = function(req, res, next) {
     const data =  req.body;
+    console.log(data);
     return res.status(200).send(data);
 
   /*  if(data) {        
