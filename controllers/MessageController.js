1337885968ee725899bb6f30c97ff8c999493bdd
ayoub <ayoub.belghar@gmail.com>
2018-06-30 01:11:48 +0100

    purge and save messages inoutbox

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -7,52 +7,58 @@ exports.saveMessagesInbox = function(req, res, next){
     const data = JSON.parse(req.body.data);
     console.log(data);
     console.log("SAVE Messages Inbox");
-    async.forEachOf(data, (value, key, callback) => {        
-        let message = new MessageModel({
-            idEnfant: value.key,
-            date: new Date(parseInt(value.date)),
-            numero: value.numero,
-            message_body: value.body,
-            type_message: 'inbox'
-        });
-        console.log('message',message);
-        
-        message.save(function(err, newMessage) {
-            if(err) return callback(err);
-            console.log("NEW",newMessage);
-            callback();
+    MessageModel.remove({ type_message: 'inbox' }, function(){
+        async.forEachOf(data, (value, key, callback) => {        
+            let message = new MessageModel({
+                idEnfant: value.key,
+                date: new Date(parseInt(value.date)),
+                numero: value.numero,
+                message_body: value.body,
+                type_message: 'inbox'
+            });
+            console.log('message',message);
+            
+            message.save(function(err, newMessage) {
+                if(err) return callback(err);
+                console.log("NEW",newMessage);
+                callback();
+            })
+        }, err => {
+            if(err) console.error(err.message);        
+            console.log('all messages are saved');      
+            return res.status(200).send(data);  
         })
-    }, err => {
-        if(err) console.error(err.message);        
-        console.log('all messages are saved');      
-        return res.status(200).send(data);  
-    })        
+    })
+            
 }
 
 exports.saveMessagesOutbox = function(req, res, next){
     const data = JSON.parse(req.body.data);
     console.log(data);
     console.log("SAVE Messages Inbox");
-    async.forEachOf(data, (value, key, callback) => {        
-        let message = new MessageModel({
-            idEnfant: value.key,
-            date: new Date(parseInt(value.date)),
-            numero: value.numero,
-            message_body: value.body,
-            type_message: 'outbox'
-        });
-        console.log('message',message);
-        
-        message.save(function(err, newMessage) {
-            if(err) return callback(err);
-            console.log("NEW",newMessage);
-            callback();
+    MessageModel.remove({ type_message: 'outbox' }, function(){
+        async.forEachOf(data, (value, key, callback) => {        
+            let message = new MessageModel({
+                idEnfant: value.key,
+                date: new Date(parseInt(value.date)),
+                numero: value.numero,
+                message_body: value.body,
+                type_message: 'outbox'
+            });
+            console.log('message',message);
+            
+            message.save(function(err, newMessage) {
+                if(err) return callback(err);
+                console.log("NEW",newMessage);
+                callback();
+            })
+        }, err => {
+            if(err) console.error(err.message);        
+            console.log('all messages are saved');      
+            return res.status(200).send(data);  
         })
-    }, err => {
-        if(err) console.error(err.message);        
-        console.log('all messages are saved');      
-        return res.status(200).send(data);  
-    })        
+    })
+            
 }
 
 
